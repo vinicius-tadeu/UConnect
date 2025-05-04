066f72d798e7312f64533a3486efb4e3d4979b3f
jud-as <guilhermeferreira082@gmail.com>
2025-05-04 15:11:33 -0300

    initial commit

---
+++ src/main/java/com/example/uconnect/UconnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.uconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uconnect/UconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
